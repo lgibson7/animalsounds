8cfbdaaba305280f9dd5d0d39c3bb0c1a710736b
Lydia Gibson <l.suzanne.gibson@gmail.com>
2025-06-17 08:47:07 -0700

    add animal_sounds function documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,19 @@
-animal_sounds <- function(animal, sound) {
+#' Print a Sentence Saying What Sound an Animal Makes
+#'
+#' @param animal A character string specifying the animal
+#' @param country A character string specifying the country where the animal is found
+#' @param sound A character string specifying an animal sound
+#'
+#' @returns A character string that describes the sound the animal makes
+#' @export
+#'
+#' @examples
+#'
+#' animal_sounds("dog", "France", "woof")
+#'
+animal_sounds <- function(animal, country, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
+  stopifnot(is.character(country) & length(sound) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " goes ", sound, " ", sound, "!")
+  paste0("The ", animal, " in ", country, " goes ", sound, " ", sound, "!")
 }
